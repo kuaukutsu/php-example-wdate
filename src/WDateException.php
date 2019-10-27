e1f5cffe56688de22c8cd56ec6da984eaaad8088
Dmitriy Krivopalov <dimarik82@gmail.com>
2019-10-27 23:19:53 +0300

    fix minor

--- src/WDateException.php
+++ src/WDateException.php
@@ -12,7 +12,7 @@ class WDateException extends \LogicException
      * WDateException constructor.
      * @param string $message
      */
-    public function __construct($message = "Datetime not valid")
+    public function __construct($message = 'Datetime not valid')
     {
         parent::__construct($message);
     }
